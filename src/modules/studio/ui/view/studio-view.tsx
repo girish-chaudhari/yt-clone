0cabfa18b2c8b2ca22f139078d71ca66103b972e
girish.chaudhari <girishvishnuc98@gmail.com>
2025-03-29 12:42:17 +0530

    Enhance video section with infinite scroll, error boundary, and improved rate limiting

--- src/modules/studio/ui/view/studio-view.tsx
+++ src/modules/studio/ui/view/studio-view.tsx
@@ -1,10 +1,14 @@
-import { VideoSection } from "../sections/videos-section";
+import { VideosSection } from "../sections/videos-section";
 
 const StudioView = () => {
   return (
-    <div>
-      <VideoSection />
+    <div className="flex flex-col gap-y-6 pt-2.5">
+      <div className="px-4">
+        <h1 className="text-2xl font-bold">Channel content</h1>
+        <p className="text-xs text-muted-foreground">Manage your channel content and vidoes</p>
+      </div>
+      <VideosSection />
     </div>
   );
-}
+};
 export default StudioView;
