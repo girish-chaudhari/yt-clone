714b69f454e717e4084c4fca35fa924292d47f82
girish.chaudhari <girishvishnuc98@gmail.com>
2025-04-26 17:45:11 +0530

    feat: implement playlist details and video display; add header and video sections with dynamic data fetching

---
+++ src/modules/playlists/ui/sections/videos-section.tsx
@@ -0,0 +1,111 @@
+"use client";
+
+import { Suspense } from "react";
+import { trpc } from "@/trpc/client";
+import { DEFAULT_LIMIT } from "@/constants";
+import { ErrorBoundary } from "react-error-boundary";
+import {
+  VideoGridCard,
+  VideoGridCardSkeleton,
+} from "@/modules/videos/ui/components/video-grid-card";
+import { InfiniteScroll } from "@/components/infinite-scroll";
+import {
+  VideoRowCard,
+  VideoRowCardSkeleton,
+} from "@/modules/videos/ui/components/video-row-card";
+import { toast } from "sonner";
+
+interface VideosSectionProps {
+  playlistId: string;
+}
+
+export const VideosSection = (props: VideosSectionProps) => {
+  return (
+    <Suspense fallback={<VideosSectionSkeleton />}>
+      <ErrorBoundary fallback={<p>Error</p>}>
+        <VideosSectionSuspense {...props} />
+      </ErrorBoundary>
+    </Suspense>
+  );
+};
+
+export const VideosSectionSkeleton = () => {
+  return (
+    <div>
+      <div className="flex flex-col gap-4 gap-y-10 md:hidden">
+        {Array.from({ length: 18 }).map((_, index) => (
+          <VideoGridCardSkeleton key={index} />
+        ))}
+      </div>
+      <div className="hidden flex-col gap-4 md:flex">
+        {Array.from({ length: 18 }).map((_, index) => (
+          <VideoRowCardSkeleton size="compact" key={index} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const VideosSectionSuspense = ({ playlistId }: VideosSectionProps) => {
+  const [videos, query] = trpc.playlists.getVideos.useSuspenseInfiniteQuery(
+    {
+      playlistId,
+      limit: DEFAULT_LIMIT,
+    },
+    {
+      getNextPageParam: (lastPage) => lastPage.nextCursor,
+    },
+  );
+
+  const utils = trpc.useUtils();
+
+  const removeVideo = trpc.playlists.removeVideo.useMutation({
+    onSuccess: (data) => {
+      toast.success("Video removed from playlist");
+      utils.playlists.getMany.invalidate();
+      utils.playlists.getManyForVideo.invalidate({ videoId: data.videoId });
+      utils.playlists.getOne.invalidate({ id: data.playlistId });
+      utils.playlists.getVideos.invalidate({ playlistId: data.playlistId });
+    },
+    onError: () => {
+      toast.error("Something went wrong");
+    },
+  });
+
+  return (
+    <div>
+      <div className="flex flex-col gap-4 gap-y-10 md:hidden">
+        {videos.pages
+          .flatMap((page) => page.items)
+          .map((video) => (
+            <VideoGridCard
+              data={video}
+              key={video.id}
+              onRemove={() =>
+                removeVideo.mutate({ playlistId, videoId: video.id })
+              }
+            />
+          ))}
+      </div>
+      <div className="hidden flex-col gap-4 md:flex">
+        {videos.pages
+          .flatMap((page) => page.items)
+          .map((video) => (
+            <VideoRowCard
+              data={video}
+              key={video.id}
+              size="compact"
+              onRemove={() =>
+                removeVideo.mutate({ playlistId, videoId: video.id })
+              }
+            />
+          ))}
+      </div>
+      <InfiniteScroll
+        hasNextPage={query.hasNextPage}
+        isFetchingNextPage={query.isFetchingNextPage}
+        fetchNextPage={query.fetchNextPage}
+      />
+    </div>
+  );
+};
