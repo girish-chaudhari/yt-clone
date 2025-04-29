e446a300d66db4dc52a993ce16d00a73a440ced8
girish.chaudhari <girishvishnuc98@gmail.com>
2025-04-29 20:27:55 +0530

    Set dynamic rendering to 'force-dynamic' for the studio page component

--- src/app/(studio)/studio/page.tsx
+++ src/app/(studio)/studio/page.tsx
@@ -3,6 +3,8 @@ import { HydrateClient, trpc } from "@/trpc/server";
 
 import StudioView from "@/modules/studio/ui/views/studio-view";
 
+export const dynamic = 'force-dynamic';
+
 const Page = async () => {
   void trpc.studio.getMany.prefetchInfinite({
     limit: DEFAULT_LIMIT,
